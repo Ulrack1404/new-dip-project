133b66f85da925da78ff84492d811b0f9873b8a0
Denis <peredelkin.d@gmail.com>
2023-02-19 09:59:50 +0300

    доработка initDatabase

--- server/startUp/initDatabase.js
+++ server/startUp/initDatabase.js
@@ -5,14 +5,14 @@ const foodsMock = require("../mock/foods.json");
 const categoriesMock = require("../mock/categories.json");
 
 module.exports = async () => {
-    const foods = await Food.find();
-    if (foods.length !== foodsMock.length) {
-        await createInitialEntity(Food, foodsMock);
-    }
-
     const categories = await Category.find();
     if (categories.length !== categoriesMock.length) {
         await createInitialEntity(Category, categoriesMock);
+    }
+
+    const foods = await Food.find();
+    if (foods.length !== foodsMock.length) {
+        await createInitialEntity(Food, foodsMock);
     }
 };
 
@@ -22,7 +22,14 @@ async function createInitialEntity(Model, data) {
         data.map(async (item) => {
             try {
                 delete item._id;
+                if (item.hasOwnProperty("category")) {
+                    const category = await Category.findOne({
+                        name: [item.category]
+                    });
+                    item.category = category._id;
+                }
                 const newItem = new Model(item);
+
                 await newItem.save();
                 return newItem;
             } catch (e) {
